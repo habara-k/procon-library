9e57c870de7c56d9b48222381e4a02037d645745
Keigo Habara <k.habara.aa@gmail.com>
2020-03-04 20:03:13 +0900

    Add SCC

---
+++ lib/graph/strongly_connected_components.cpp
@@ -0,0 +1,45 @@
+#include "../template.cpp"
+
+struct StronglyConnectedComponents {
+    vector<vector<int>> g, rg, t;
+    vector<int> comp, ord, used;
+
+    StronglyConnectedComponents(const vector<vector<int>>& g) :
+        g(g), rg(g.size()), comp(g.size(), -1), used(g.size())
+    {
+        for (int i = 0; i < g.size(); i++) {
+            for (int to : g[i]) {
+                rg[to].push_back(i);
+            }
+        }
+    }
+
+    void dfs(int idx) {
+        if (used[idx]) return;
+        used[idx] = true;
+        for (int to : g[idx]) dfs(to);
+        ord.push_back(idx);
+    }
+
+    void rdfs(int idx, int cnt) {
+        if (comp[idx] != -1) return;
+        comp[idx] = cnt;
+        for (int to : rg[idx]) rdfs(to, cnt);
+    }
+
+    void build() {
+        for (int i = 0; i < g.size(); i++) dfs(i);
+        reverse(ord.begin(), ord.end());
+        int ptr = 0;
+        for (int i : ord) if (comp[i] == -1) rdfs(i, ptr), ptr++;
+
+        t.resize(ptr);
+        for (int i = 0; i < g.size(); i++) {
+            for (int to : g[i]) {
+                int x = comp[i], y = comp[to];
+                if (x == y) continue;
+                t[x].push_back(y);
+            }
+        }
+    }
+};
