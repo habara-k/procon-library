4e90bde0e2057f29fe39df4fd5b42fc37e47ac57
Keigo Habara <k.habara.aa@gmail.com>
2020-03-18 20:18:01 +0900

    Fix: is_prime

--- lib/number/is_prime.cpp
+++ lib/number/is_prime.cpp
@@ -2,6 +2,8 @@
 
 // O(sqrt(n))
 bool is_prime(int64_t n) {
+    assert(n > 0);
+    if (n == 1) return false;
     for (int64_t i = 2; i * i <= n; i++) {
         if (n % i == 0) return false;
     }
