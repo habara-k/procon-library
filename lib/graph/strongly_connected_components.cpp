999ea9ede6a647336fde77be0036dac4e4cbf68b
Keigo Habara <k.habara.aa@gmail.com>
2020-05-23 16:27:08 +0900

    Refactor SCC

--- lib/graph/strongly_connected_components.cpp
+++ lib/graph/strongly_connected_components.cpp
@@ -1,19 +1,41 @@
 #include "../template.cpp"
 
 struct StronglyConnectedComponents {
-    vector<vector<int>> g, rg, t;
-    vector<int> comp, ord, used;
+    int n;
+    vector<vector<int>> g, rg, graph;
+    vector<int> ord, used, comp;
 
     StronglyConnectedComponents(const vector<vector<int>>& g) :
-        g(g), rg(g.size()), comp(g.size(), -1), used(g.size())
+            n(g.size()), g(g), rg(n), used(n), comp(n, -1)
     {
-        for (int i = 0; i < g.size(); ++i) {
+        for (int i = 0; i < n; ++i) {
             for (int to : g[i]) {
                 rg[to].push_back(i);
             }
         }
     }
 
+    void build() {
+        for (int i = 0; i < n; ++i) dfs(i);
+        reverse(ord.begin(), ord.end());
+        int ptr = 0;
+        for (int i : ord) if (comp[i] == -1) rdfs(i, ptr), ptr++;
+
+        graph.resize(ptr);
+        for (int i = 0; i < n; ++i) {
+            for (int to : g[i]) {
+                int x = comp[i], y = comp[to];
+                if (x == y) continue;
+                graph[x].push_back(y);
+            }
+        }
+        for (auto& v : graph) {
+            sort(v.begin(), v.end());
+            v.erase(unique(v.begin(), v.end()), v.end());
+        }
+    }
+
+private:
     void dfs(int idx) {
         if (used[idx]) return;
         used[idx] = true;
@@ -26,20 +48,5 @@ struct StronglyConnectedComponents {
         comp[idx] = cnt;
         for (int to : rg[idx]) rdfs(to, cnt);
     }
-
-    void build() {
-        for (int i = 0; i < g.size(); ++i) dfs(i);
-        reverse(ord.begin(), ord.end());
-        int ptr = 0;
-        for (int i : ord) if (comp[i] == -1) rdfs(i, ptr), ptr++;
-
-        t.resize(ptr);
-        for (int i = 0; i < g.size(); ++i) {
-            for (int to : g[i]) {
-                int x = comp[i], y = comp[to];
-                if (x == y) continue;
-                t[x].push_back(y);
-            }
-        }
-    }
 };
+
