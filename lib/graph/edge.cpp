65c8f16b1fc1d943f3011492b17fce8dcded153b
Keigo Habara <k.habara.aa@gmail.com>
2020-06-26 12:47:02 +0900

    simplify edge

--- lib/graph/edge.cpp
+++ lib/graph/edge.cpp
@@ -6,14 +6,8 @@ template<typename T>
 struct edge {
     int src, to;
     T cost;
-
-    edge() {}
-    edge(int to, T cost) : src(-1), to(to), cost(cost) {}
-    edge(int src, int to, T cost) : src(src), to(to), cost(cost) {}
-
-    friend ostream& operator<<(ostream& os, const edge& e) {
-        return os << "(" << e.src << "->" << e.to << ":" << e.cost << ")";
-    }
 };
+
+using Graph = vector<vector<edge<T>>>;
 
 #endif
