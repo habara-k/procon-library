4da3ca1a5d447303b8d40ca83fa1d6d2e284d140
Keigo Habara <k.habara.aa@gmail.com>
2021-02-25 10:14:17 +0900

    Update template

--- lib/template.cpp
+++ lib/template.cpp
@@ -3,68 +3,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// type {{{
-using ll = long long;
-using ull = unsigned long long;
-using ld = long double;
-using P = pair<int,int>;
-using vi = vector<int>;
-using vll = vector<ll>;
-using vvi = vector<vector<int>>;
-using vvll = vector<vector<ll>>;
-// }}}
-
-
-// macro {{{
 #define REP(i,n) for (int i=0; i<(n); ++i)
-#define RREP(i,n) for (int i=(int)(n)-1; i>=0; --i)
-#define FOR(i,a,n) for (int i=(a); i<(n); ++i)
-#define RFOR(i,a,n) for (int i=(int)(n)-1; i>=(a); --i)
+#define ALL(x) begin(x),end(x)
+#define DUMP(x) cerr<<#x<<" = "<<(x)<<endl
 
-#define SZ(x) ((int)(x).size())
-#define all(x) begin(x),end(x)
-// }}}
+using ll = long long;
 
-
-// debug {{{
-#define dump(x) cerr<<#x<<" = "<<(x)<<endl
-#define debug(x) cerr<<#x<<" = "<<(x)<<" (L"<<__LINE__<<")"<< endl;
-
-template<typename T>
-ostream& operator<<(ostream& os, const vector<T>& v) {
-    os << "[";
-    REP (i, SZ(v)) {
-        if (i) os << ", ";
-        os << v[i];
-    }
-    return os << "]";
-}
-
-template<typename T, typename U>
-ostream& operator<<(ostream& os, const pair<T, U>& p) {
-    return os << "(" << p.first << " " << p.second << ")";
-}
-// }}}
-
-
-// chmax, chmin {{{
 template<class T>
-bool chmax(T& a, const T& b) {
+bool chmin(T &a, T b) {
+    if (a > b) { a = b; return true; }
+    return false;
+}
+template<class T>
+bool chmax(T &a, T b) {
     if (a < b) { a = b; return true; }
     return false;
 }
+
 template<class T>
-bool chmin(T& a, const T& b) {
-    if (b < a) { a = b; return true; }
-    return false;
+ostream &operator<<(ostream &os, const vector<T> &v);
+template<class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p);
+template<class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &mp);
+
+template<class T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+    os << '[';
+    REP(i, v.size()) {
+        if (i) os << ',';
+        os << v[i];
+    }
+    return os << ']';
 }
-// }}}
 
+template<class T, class U>
+ostream &operator<<(ostream &os, const pair<T, U> &p) {
+    return os << '(' << p.first << ' ' << p.second << ')';
+}
 
-// constants {{{
-#define inf(T) (numeric_limits<T>::max() / 2)
-const ll MOD = 1e9+7;
-const ld EPS = 1e-9;
-// }}}
+template<class T, class U>
+ostream &operator<<(ostream &os, const map<T, U> &mp) {
+    os << '{';
+    for (const auto &p : mp) {
+        os << p << endl;
+    }
+    return os << '}';
+}
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+struct edge {
+    int to, cost;
+};
 
 #endif
